0538a19591ba044a7de1b412e9c25365584bdf40
Vatsal Gupta <vatsalgupta280@gmail.com>
2021-12-30 22:49:10 +0530

    added scrolling, multiple cell selecting, cell formatting

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Constants
-const rows = 20;
-const cols = 20
+const rows = 100;
+const cols = 50;
 
 $(document).ready(() => {
     addColNames(cols);
@@ -39,25 +39,88 @@ const addRowNames = rows => {
 const addCells = (rows, cols) => {
     const inputCellContainer = $(".input-cell-container");
     for (let i = 1; i <= rows; i++) {
-        const cellRow = $(`<div class="cell-row" id="rowId-${i}"></div>`);
+        const cellRow = $(`<div class="cell-row rowId-${i}"></div>`);
         for (let j = 1; j <= cols; j++)
-            $(`<div class="input-cell" id="colId-${j}"></div>`).appendTo(cellRow);
+            $(`<div class="input-cell colId-${j}"></div>`).appendTo(cellRow);
         cellRow.appendTo(inputCellContainer);
     }
 }
 
+const getRowCol = e => {
+    const row = $(e.currentTarget).parent().attr("class").split(" ").filter(str => str.includes("rowId-"))[0].split("-")[1];
+    const col = $(e.currentTarget).attr("class").split(" ").filter(str => str.includes("colId-"))[0].split("-")[1];
+    return [parseInt(row), parseInt(col)];
+}
+
 const addEventListeners = () => {
-    $(".align-icon").click(event => {
+    $(".icon-bold").click(e => {
+        const selected = $(e.currentTarget).hasClass("selected");
+        $(".input-cell.selected").css("font-weight", selected ? "" : "bold");
+    });
+
+    $(".icon-italic").click(e => {
+        const selected = $(e.currentTarget).hasClass("selected");
+        $(".input-cell.selected").css("font-style", selected ? "" : "italic");
+    });
+
+    $(".icon-underline").click(e => {
+        const selected = $(e.currentTarget).hasClass("selected");
+        $(".input-cell.selected").css("text-decoration", selected ? "" : "underline");
+    });
+
+    $(".align-icon").click(e => {
         $(".align-icon.selected").removeClass("selected");
-        $(event.currentTarget).addClass("selected");
+        $(e.currentTarget).addClass("selected");
     });
 
-    $(".style-icon").click(event => {
-        $(event.currentTarget).toggleClass("selected");
+    $(".style-icon").click(e => {
+        $(e.currentTarget).toggleClass("selected");
     });
 
-    $(".input-cell").click(event => {
+    $(".input-cell").click(e => {
+        if (e.ctrlKey) {
+            const [rowId, colId] = getRowCol(e);
+            const topCellSelected = rowId > 1 && $(`.rowId-${rowId - 1} .colId-${colId}`).hasClass("selected");
+            if (topCellSelected) {
+                $(e.currentTarget).addClass("top-cell-selected");
+                $(`.rowId-${rowId - 1} .colId-${colId}`).addClass("bottom-cell-selected");
+            }
+            const bottomCellSelected = rowId < rows && $(`.rowId-${rowId + 1} .colId-${colId}`).hasClass("selected")
+            if (bottomCellSelected) {
+                $(e.currentTarget).addClass("bottom-cell-selected");
+                $(`.rowId-${rowId + 1} .colId-${colId}`).addClass("top-cell-selected");
+            }
+            const leftCellSelected = colId > 1 && $(`.rowId-${rowId} .colId-${colId - 1}`).hasClass("selected");
+            if (leftCellSelected) {
+                $(e.currentTarget).addClass("left-cell-selected");
+                $(`.rowId-${rowId} .colId-${colId - 1}`).addClass("right-cell-selected");
+            }
+            const rightCellSelected = rowId < cols && $(`.rowId-${rowId} .colId-${colId + 1}`).hasClass("selected")
+            if (rightCellSelected) {
+                $(e.currentTarget).addClass("right-cell-selected");
+                $(`.rowId-${rowId} .colId-${colId + 1}`).addClass("left-cell-selected");
+            }
+        } else {
+            $(".input-cell.selected").removeClass("selected");
+            $(".input-cell.top-cell-selected").removeClass("top-cell-selected");
+            $(".input-cell.bottom-cell-selected").removeClass("bottom-cell-selected");
+            $(".input-cell.left-cell-selected").removeClass("left-cell-selected");
+            $(".input-cell.right-cell-selected").removeClass("right-cell-selected");
+        }
+        $(`.input-cell[contenteditable="true"]`).removeAttr("contenteditable");
+        $(e.currentTarget).addClass("selected");
+    });
+
+    $(".input-cell").dblclick(e => {
         $(".input-cell.selected").removeClass("selected");
-        $(event.currentTarget).addClass("selected");
+        $(e.currentTarget).addClass("selected");
+        $(`.input-cell[contenteditable="true"]`).removeAttr("contenteditable");
+        $(e.currentTarget).attr("contenteditable", "true");
+        $(e.currentTarget).focus();
+    });
+
+    $(".input-cell-container").scroll(e => {
+        $(".column-name-container").scrollLeft(e.currentTarget.scrollLeft);
+        $(".row-name-container").scrollTop(e.currentTarget.scrollTop);
     });
 }
